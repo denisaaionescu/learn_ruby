77f6bb0035967010132d5a671dbf8714b14c4756
Denisa Ionescu <ionescudenisa12@yahoo.com>
2026-07-06 20:37:31 +0300

    Refactor loop and add rubocop dependency

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,26 +1,25 @@
-def translate(word)
-    vowels = ["a", "e", "i", "o", "u"]
-    words = word.split(" ")
+def translate(sentence)
+  vowels = %w[a e i o u]
+  words = sentence.split(' ')
 
-    for i in 0...words.length
-        w = words[i]
-        if vowels.include?(w[0])
-            words[i] = w + "ay"
-        else
-            consonants = ""
-            j = 0
-            while j < w.length && !vowels.include?(w[j])
-                consonants += w[j]
-                if w[j] == "q" && w[j + 1] == "u"
-                    consonants += "u"
-                    j += 1
-                end
-                j += 1
-            end
-            rest = w[consonants.length..-1]
-            words[i] = rest + consonants + "ay"
+  words = sentence.split(' ').map.with_index do |word, i|
+    if vowels.include?(word[0])
+      word + 'ay'
+    else
+      consonants = ''
+      j = 0
+      while j < word.length && !vowels.include?(word[j])
+        consonants += word[j]
+        if word[j] == 'q' && word[j + 1] == 'u'
+          consonants += 'u'
+          j += 1
         end
+        j += 1
+      end
+      rest = word[consonants.length..-1]
+      words[i] = rest + consonants + 'ay'
     end
+  end
 
-    words.join(" ")
+  words.join(' ')
 end
