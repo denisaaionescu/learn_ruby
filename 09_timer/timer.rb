99a3c1e1f2c9e167443d7dd3134cdee7d16f0317
Denisa Ionescu <ionescudenisa12@yahoo.com>
2026-07-06 16:39:30 +0300

    Fix conversion using format function and added costants

--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -1,28 +1,24 @@
 class Timer
+  SECONDS_IN_MINUTE = 60
+  SECONDS_IN_HOUR = 60 * 60
+  attr_accessor :seconds
+
   def initialize
     @seconds = 0
   end
 
-  def seconds
-    @seconds
-  end
-
-  def seconds=(s)
-    @seconds = s
-  end
-
   def padded(n)
-    if n < 10
-      "0#{n}"
+    if n >= 0 && n < 10
+      format("%02d", n)
     else
       n.to_s
     end
   end
 
   def time_string
-    hours = @seconds / 3600
-    minutes = (@seconds % 3600) / 60
-    secs = @seconds % 60
+    hours = @seconds / SECONDS_IN_HOUR
+    minutes = (@seconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    secs = @seconds % SECONDS_IN_MINUTE
 
     "#{padded(hours)}:#{padded(minutes)}:#{padded(secs)}"
   end
